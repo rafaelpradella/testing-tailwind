8e9f56fd7f03af68131b3720389c6906b1c1b3ba
Rafael Pradella <pradella@outlook.com>
2021-08-20 22:38:03 -0300

    Configuring Tailwind w/ PostCSS nesting

---
+++ craco.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  style: {
+    postcss: {
+      plugins: [
+        require('postcss-import'),
+        require('tailwindcss/nesting'),
+        require('tailwindcss'),
+        require('autoprefixer'),
+      ],
+    },
+  },
+}
